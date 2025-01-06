eadcaa0dfa28e608f54cd7e80ae9e80aee7979bc
unknown <katotodan@gmail.com>
2025-01-06 13:42:46 +0300

    socket change

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,7 @@
 import { io } from 'socket.io-client';
 
-const serverUrl = process.env.REACT_APP_API_URL
-const wssServerUrl = "wss" + serverUrl.slice(5)
 
-export const socket = io(wssServerUrl, {
+export const socket = io(process.env.REACT_APP_API_URL, {
     withCredentials: true,
     headers: {
     'Content-Type': 'application/json',
